ccea9cdff4c734e1c0ff93986d49cbcce93bc4f9
Prasad <Prasad@gmail.com>
2026-09-18 08:21:14 +0530

    Fix MongoDB Atlas DNS connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const app = require("./app");
 const connectDB = require("./config/db");
 const adminRoutes = require("./routes/adminRoutes");
